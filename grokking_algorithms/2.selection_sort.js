3fc48305afb52a0d48bb562ac99ed39b29a5966f
Colin Sidberry <colinsidberry@gmail.com>
2021-12-19 12:46:12 -0500

    adjusting naming convention and starting chapter 2 problems.

---
+++ grokking_algorithms/2.selection_sort.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// find the smallest item in an arr
+    // assume that the first item is the smallest and assign it and the index as such
+    // loop through the arr starting at position 1
+        // if that item is smaller adjust smallest item
+    // return the smallest item and mutate the arr so that the smallest item is removed
+
+// run the find smallest item function for the length of the arr adding the smallest item to a new arr
