d86bf085eacd615c1adae915e9992acd73abaf8b
Colin Sidberry <colinsidberry@gmail.com>
2021-12-24 15:35:38 -0500

    improving test to test deep equality.

--- leetcode/mergeIntervals.test.js
+++ leetcode/mergeIntervals.test.js
@@ -2,12 +2,19 @@
 
 const { merge } = require("./mergeIntervals");
 
-describe("testing countDown recursive function", () => {
-    it("counts down properly", () => {
+describe("testing mergeIntervals function", () => {
+    it("merges multiple subarrays properly", () => {
         let input = [[1, 3], [2, 6], [8, 10], [15, 18]];
         let output = merge(input);
+        let expected = [[1, 6], [8, 10], [15, 18]];
 
-        expect(output[0][1]).toEqual(6); //Output: [[1,6],[8,10],[15,18]]
-        expect(output[2][1]).toEqual(18);
+        // expect(output[0][1]).toEqual(6); //Output: [[1,6],[8,10],[15,18]]
+        // expect(output[2][1]).toEqual(18);
+        expect(output.every(
+            (a, i) => a.every(
+                (b, j) => b === expected[i][j]
+            )
+        )).toEqual(true);
+        // a.every((val, index) => val === b[index]);
     });
 });
